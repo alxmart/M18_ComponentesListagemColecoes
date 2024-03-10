d3cddb26b1f765c9e0577e87ae77040d556cfafb
alxmart <alxmartinez@gmail.com>
2024-03-10 15:15:26 -0300

    Exemplo criando  Classe id, dentro da Classe Recursos

--- app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.luizafmartinez.m18_componenteslistagemcolecoes
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -22,12 +23,23 @@ class MainActivity : AppCompatActivity() {
         btnExecutar = findViewById(R.id.btn_executar)
         textResultado = findViewById(R.id.text_resultado)
 
-        println(Recursos.id.text_numero); // Classe id, dentro da Classe Recursos
+        //println(Recursos.id.text_numero); // Classe id, dentro da Classe Recursos
+
+      /*  btnExecutar.setOnClickListener {
+            textResultado.text = "jamilton"
+        }*/
 
         /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
+    }
+
+    fun cliqueBotao(view: View) {
+
+        btnExecutar.setOnClickListener {
+            textResultado.text = "jamilton"
+        }
     }
 }
