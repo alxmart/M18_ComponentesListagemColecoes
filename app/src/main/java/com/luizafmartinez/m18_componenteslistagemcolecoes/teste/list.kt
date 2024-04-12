97d8ac37f125e46a8588848534769c6af817468c
alxmart <alxmartinez@gmail.com>
2024-04-12 09:07:31 -0300

    Tests using mutableListOf

---
+++ app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/teste/list.kt
@@ -0,0 +1,42 @@
+package com.luizafmartinez.m18_componenteslistagemcolecoes.teste
+
+
+
+fun main() {
+
+    //val listaArray = arrayOf("jamilton", "ana")//Não pode adicionar mais items
+
+    //listaArray[0] = "Jamilton Alterado"
+
+    //val listaARray: List<String> = listOf("jamilton", "ana")
+
+    /*val lista = mutableListOf("jamilton","ana")
+
+    lista[0] = "Jamilton Alterado"
+
+    lista.add("márcia")
+
+    lista.forEach { item ->
+        println(item)
+    }
+    // Jamilton Alterado / ana / márcia
+
+    val novaLista = listOf("marcelo","marcela")
+    lista.addAll(novaLista)
+
+    lista.forEach { item ->
+        println(item)
+    }
+    //Jamilton Alterado / ana / márcia / marcelo / marcela
+
+    //Para remover Ana:
+
+    // lista.remove("ana")
+    lista.removeAt(1) // Remove posicao 1 : ana
+    lista.forEach { item ->
+        println(item)
+    }*/
+
+
+
+}
