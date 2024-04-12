cf0400259bb163260aea84deb1f78ae67fba4040
alxmart <alxmartinez@gmail.com>
2024-04-12 13:34:59 -0300

    Tests using mutableSetOf

--- app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/teste/set.kt
+++ app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/teste/set.kt
@@ -8,10 +8,28 @@ data class Carro(
 fun main() {
 
     // SET - Lista de itens que não se repetem
-    val lista = setOf("jamilton", "ana", "pedro", "maria", "ana")
+    //val lista = setOf("jamilton", "ana", "pedro", "maria", "ana")
 
-    lista.forEach { nome ->
+    //val lista = mutableSetOf("jamilton", "ana", "pedro", "maria", "ana")
+
+    val lista = mutableSetOf(
+        Carro("Evoque","Land Rover"),
+        Carro("Navigator","Lincoln"),
+        Carro("Charger","Dodge")
+    )
+
+    //lista.add("Mariana")
+
+    //lista.remove("ana")
+
+    // lista.clear()
+
+    /*lista.forEach { nome ->
         println( nome )
+    }*/
+
+    lista.forEach { car ->
+        println( car )
     }
     // Obs: Não se utiliza indices para fazer exibição. println(lista[0])
 
@@ -19,7 +37,7 @@ fun main() {
 
     println("Tamanho da lista: ${lista.size} itens")
 
-    println( lista.indexOf("jamilton") )
+    //println( lista.indexOf("jamilton") )
 
     // ==================================================
     //  MutableSetOf
