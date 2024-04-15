adb50372bcff68c373d81f892cbffc9ad9b4eed8
alxmart <alxmartinez@gmail.com>
2024-04-15 18:57:08 -0300

    Tests using union method.

---
+++ app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/teste/metodo_union.kt
@@ -0,0 +1,27 @@
+package com.luizafmartinez.m18_componenteslistagemcolecoes.teste
+
+
+fun main() {
+
+    val listaLanches = listOf("Hamburguer", "Batata frita")
+    val listaEntradas = listOf("Coxa de frango", "Pipoca")
+
+    val novaLista = listaLanches.union(listaEntradas)
+    println(novaLista)
+
+    val novaLista2 = listaEntradas + listaLanches
+    println(novaLista2)
+
+    val listaExclusao = listOf("Batata frita")
+
+    val listaFinal = novaLista2 - listaExclusao
+    println(listaFinal)
+
+
+
+
+
+
+
+
+}
