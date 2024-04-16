111ea1c5d8fd503b3f46a328ea31129bd6e95e82
alxmart <alxmartinez@gmail.com>
2024-04-16 19:22:22 -0300

    Tests using Interface

--- app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/teste/InterfacePratica.kt
+++ app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/teste/InterfacePratica.kt
@@ -1,55 +1,97 @@
 package com.luizafmartinez.m18_componenteslistagemcolecoes.teste
 
-// DRY -> Don't repeat yourself / Não repita código !
-// Classes -> Divisão de responsabilidade
-// Alto Acoplamento e Baixo Acoplamento
-// Classes devem utilizar outras classes   ( "... e não criar outras classes )
+// DRY -> Don't repeat yourself / Não repita código ! (ok)
+// Classes -> Divisão de responsabilidade (ok)
+// Classes devem utilizar outras classes  ("...e não criar outras classes") (ok)
+// Alto Acoplamento e Baixo Acoplamento (ok)
 
-class Musico {
+class Musico( val instrumento: Instrumento ) {  // Passando um objeto no construtor
 
     fun tocar() {
-        val violao = Violao()   // Alto Acoplamento
-        // Classes devem utilizar outras classes   ( "... e não criar outras classes )
+        //val violao = Violao()   // Alto Acoplamento
+        // Classes devem utilizar outras classes  ("...e não criar outras classes)
         println("Músico tocando")
-        violao.sendoTocado()
+        instrumento.sendoTocado()
+        //instrumento.tocandoBateria()  // Bateria tem outro método
     }
 }
 
-class Violao {
+// Herança e Interface (Contrato) -> Composição
 
-    fun sendoTocado() {
+// HERANÇA
+/*abstract class Instrumento {
+    abstract fun sendoTocado()
+}*/
+
+// INTERFACE
+interface Instrumento {
+    fun sendoTocado()
+}
+
+// Herança:
+// class Violao : Instrumento() {//Dois tipos: Violao(class) e Instrumento(Interface)
+// Interface:
+class Violao : Instrumento {  // Dois tipos: Violao e Instrumento
+
+    override fun sendoTocado() {
         println("Utilizando cordas (20 linhas de código)")
         println("para tocar o violão")
     }
 
+    fun ajustarCordas() {
+        println("Ajustar cordas")
+    }
 }
+// Herança:
+//class Bateria : Instrumento() {  // Dois tipos: Bateria e Instrumento
+// Interface:
+class Bateria : Instrumento {  // Dois tipos: Bateria e Instrumento
 
-class Bateria {
-
-    fun tocandoBateria() {
-
+    override fun sendoTocado() {
         println("Utilizando baquetas (20 linhas de código)")
         println("para tocar a bateria")
     }
+
+    fun ajustarBaqueta() {
+        println("Ajustar baqueta")
+    }
 }
+
+    class Guitarra : Instrumento{
+
+        override fun sendoTocado() {
+            println("Utilizando cordas (20 linhas de código)")
+            println("Utilizando alavanca")
+            println("para tocar a guitarra")
+        }
+    }
 
 fun main() {
 
+    //val violao : Instrumento = Violao()
+    //val bateria: Instrumento = Bateria()
+
     // Tela 1 (Violão)
-    val musico1 = Musico()
+    val violao1 = Violao()
+    val musico1 = Musico( violao1 )
     musico1.tocar()
     println("++++++++++++++++++++++++++++++")
 
     // Tela 2 (Violão)
-    val musico2 = Musico()
+    val violao2 = Violao()
+    val musico2 = Musico( violao2 )
     musico2.tocar()
     println("++++++++++++++++++++++++++++++")
 
     // Tela 3 (Bateria)
-    val musico3 = Musico()
+    val bateria1 = Bateria()
+    val musico3 = Musico( bateria1 ) //Bateria como Instrumento(métodos de intrumento)
     musico3.tocar()
     println("++++++++++++++++++++++++++++++")
 
-
+    // Tela 4 (Guitarra)
+    val guitarra = Guitarra()
+    val musico4 = Musico( guitarra ) // Dois tipos: Guitarra e instrumento
+    musico4.tocar()
 
 }
