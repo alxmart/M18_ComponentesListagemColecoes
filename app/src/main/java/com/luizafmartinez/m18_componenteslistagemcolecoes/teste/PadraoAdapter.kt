6c509ceabf6baa1e2faee59cbfa0ff1814988d24
alxmart <alxmartinez@gmail.com>
2024-04-18 16:13:05 -0300

    Tests using Adapter (Adapter pattern)

--- app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/teste/PadraoAdapter.kt
+++ app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/teste/PadraoAdapter.kt
@@ -1,11 +1,8 @@
 package com.luizafmartinez.m18_componenteslistagemcolecoes.teste
 
 class TomadaAntiga( val conector: Conector) {
-
     fun passarEnergia() {
-
         val qtdPinos = conector.quantidadePinos()
-
         if (qtdPinos == 2) {
             conector.ligarAparelho()
             println("Quantidade de pinos: $qtdPinos")
@@ -17,18 +14,14 @@ class TomadaAntiga( val conector: Conector) {
 }
 
 interface Conector {
-
     fun quantidadePinos() : Int
-
     fun ligarAparelho()
 }
 
 class ConectorNovoPadrao : Conector {
-
     override fun quantidadePinos() : Int {
         return 3
     }
-
     override fun ligarAparelho() {
         println("Aparelho está ligado")
     }
@@ -37,9 +30,7 @@ class ConectorNovoPadrao : Conector {
 fun main() {
 
     val conectorNovoPadrao = ConectorNovoPadrao()
-
     val tomadaAntiga = TomadaAntiga( conectorNovoPadrao )
-
     tomadaAntiga.passarEnergia()
     
 }
