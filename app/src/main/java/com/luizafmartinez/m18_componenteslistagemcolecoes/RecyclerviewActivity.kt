425733e0abcb9ca6d9009bd79113784bc956960d
alxmart <alxmartinez@gmail.com>
2024-04-23 15:49:05 -0300

    Tests using LayoutManager StaggeredGrid Layout

--- app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/RecyclerviewActivity.kt
+++ app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/RecyclerviewActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 class RecyclerviewActivity : AppCompatActivity() {
 
@@ -24,7 +26,9 @@ class RecyclerviewActivity : AppCompatActivity() {
 
         val lista = listOf(
             Mensagem("Jamilton","Olá, tudo bem ?","10:45"),
-            Mensagem("Ana", "Te vi ontem..", "00:45"),
+            //Testando o StaggeredGrid Layout: (texto grande)
+            Mensagem("Ana", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum", "00:45"),
+            //Mensagem("Ana", "Te vi ontem..", "00:45"),
             Mensagem("Maria","Não acredito...","06:03"),
             Mensagem("Pedro","Futebol hoje ?","15:32")
         )
@@ -32,9 +36,20 @@ class RecyclerviewActivity : AppCompatActivity() {
         rvLista = findViewById(R.id.rv_lista)
         rvLista.adapter = MensagemAdapter(lista)//Tem que ser do tipo "MensagemAdapter" e "Adapter"
 
+        // StaggeredGrid Layou MAnager
+        rvLista.layoutManager = StaggeredGridLayoutManager(
+            2,
+            RecyclerView.VERTICAL
+        )
+
+        // Grid Layout Manager
+        /*
+        rvLista.layoutManager = GridLayoutManager(
+            this,
+            2
+        )*/
+
         // Linear Layout Manager (Configurar com XML ou Código)
-        // XML:
-        // app:layoutManager="androidx.recyclerview.widget.LinearLayoutManager"
         // Código Kotlin:
         /*rvLista.layoutManager = LinearLayoutManager(
             this,
