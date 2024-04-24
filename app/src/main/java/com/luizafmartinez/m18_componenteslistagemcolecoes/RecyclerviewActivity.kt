8d1758d4ad05d0ce61c63184b22ac13cfc48f881
alxmart <alxmartinez@gmail.com>
2024-04-24 17:33:32 -0300

    Testing onClicListener in CardView

--- app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/RecyclerviewActivity.kt
+++ app/src/main/java/com/luizafmartinez/m18_componenteslistagemcolecoes/RecyclerviewActivity.kt
@@ -7,12 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.luizafmartinez.m18_componenteslistagemcolecoes.teste.nome
 
 class RecyclerviewActivity : AppCompatActivity() {
 
@@ -29,27 +25,31 @@ class RecyclerviewActivity : AppCompatActivity() {
         }
 
         val lista = listOf(
-            Mensagem("Jamilton","Olá, tudo bem ?","10:45"),
+            Mensagem("Jamilton", "Olá, tudo bem ?", "10:45"),
             //Testando o StaggeredGrid Layout: (texto grande)
-            Mensagem("Ana", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum", "00:45"),
+            Mensagem(
+                "Ana",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+                "00:45"
+            ),
             //Mensagem("Ana", "Te vi ontem..", "00:45"),
-            Mensagem("Maria","Não acredito...","06:03"),
-            Mensagem("Pedro","Futebol hoje ?","15:32")
+            Mensagem("Maria", "Não acredito...", "06:03"),
+            Mensagem("Pedro", "Futebol hoje ?", "15:32")
         )
 
         rvLista = findViewById(R.id.rv_lista)
 
         //Tem que ser do tipo "MensagemAdapter" e "Adapter"
-        rvLista.adapter = MensagemAdapter( lista ) { nome ->
+        rvLista.adapter = MensagemAdapter(lista) { nome ->
 
-          Toast.makeText(this,"Olá $nome",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Olá $nome", Toast.LENGTH_SHORT).show()
 
-          val intent = Intent(this, ListViewActivity::class.java)
-          intent.putExtra("nome", nome)
+            val intent = Intent(this, ListViewActivity::class.java)
+            intent.putExtra("nome", nome)
 
-          startActivity(
-              Intent(this, ListViewActivity::class.java)
-          )
+            startActivity(
+                Intent(this, ListViewActivity::class.java)
+            )
 
         }
 
